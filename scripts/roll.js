3977bbe27d4ef00955daf13288edabc5e34e0d11
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2015-07-06 15:09:56 -0400

    Don't use / since slackbot intercepts those

--- scripts/roll.js
+++ scripts/roll.js
@@ -1,5 +1,5 @@
 module.exports = function(robot){
-    robot.hear(/^\/roll (\d+)/i,function(msg){
+    robot.hear(/^roll (\d+)/i,function(msg){
         randomInt = Math.floor(Math.random() * msg.match[1]);
         msg.send("you rolled " + randomInt);
     });
