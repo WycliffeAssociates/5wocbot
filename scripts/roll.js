f5e350c1f095ef5fdf42bde156b3cf9e47f86aa8
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2015-07-07 10:47:51 -0400

    1 is now the minimum

--- scripts/roll.js
+++ scripts/roll.js
@@ -1,6 +1,6 @@
 module.exports = function(robot){
     robot.respond(/roll (\d+)/i,function(msg){
-        randomInt = Math.floor(Math.random() * msg.match[1]);
+        randomInt = Math.floor(Math.random() * msg.match[1]) + 1;
         msg.send("you rolled the number " + randomInt);
     });
 };
