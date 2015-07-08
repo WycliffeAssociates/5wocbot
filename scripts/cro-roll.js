9f7140a44e9027995c94a88c7490247e3efe47a3
Craig Oliver <craig_oliver@wycliffeassociates.org>
2015-07-08 10:26:32 -0400

    Refactor rolling into its own function

--- scripts/cro-roll.js
+++ scripts/cro-roll.js
@@ -1,23 +1,24 @@
-/* Description:
-   Generate a random number like a dice roll
-
- Commands:
-   hubot roll N - returns a number between 1 and N
-
- Notes:
-   Currently assumes a single die, future expansion could include logic for multiple dice
-
- Author:
-   rbnswartz
+/* Inspired by Reuben's awesome roll script
 */
+
+function rollDice(numDice, numSides) {
+    var results = {
+        rolls: [],
+        total: 0
+    };
+    for (var rollNum = 0; rollNum < numDice; rollNum++) {
+        var roll = Math.floor(Math.random() * numSides + 1);
+        results.rolls.push(roll);
+        results.total += roll;
+    }
+    return results;
+}
+
 module.exports = function (robot) {
     robot.respond(/(\d+)d(\d+)/i,function(msg){
         var numDice = msg.match[1];
         var numSides = msg.match[2];
-        var total = 0;
-        for (var roll = 0; roll < numDice; roll++) {
-            total = total + Math.floor(Math.random() * numSides + 1);
-        }
-        msg.send("You rolled a " + total + ".");
+        var results = rollDice(numDice, numSides);
+        msg.send("You rolled a " + results.total + ". (" + results.rolls + ")");
     });
 };
