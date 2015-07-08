b204648b6d0821931f5abaaeb008276d07b4f7a8
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2015-07-08 09:58:45 -0400

    Fixed comments

--- scripts/roll.js
+++ scripts/roll.js
@@ -1,15 +1,15 @@
-# Description:
-#   Generate a random number like a dice roll
-#
-# Commands:
-#   hubot roll N - returns a number between 1 and N
-#
-# Notes:
-#   Currently assumes a single die, future expansion could include logic for multiple dice
-#
-# Author:
-#   rbnswartz
+/* Description:
+   Generate a random number like a dice roll
 
+ Commands:
+   hubot roll N - returns a number between 1 and N
+
+ Notes:
+   Currently assumes a single die, future expansion could include logic for multiple dice
+
+ Author:
+   rbnswartz
+*/
 module.exports = function (robot) {
     robot.respond(/roll (\d+)/i,function(msg){
         randomInt = Math.floor(Math.random() * msg.match[1]) + 1;
